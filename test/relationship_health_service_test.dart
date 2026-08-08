68fa30c1470b7063fd76a6f6945f9ee741a39147
joshuagrigson <jsgrigson@gmail.com>
2026-08-07 23:32:27 -0500

    test: add relationship health coverage

---
+++ test/relationship_health_service_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tether/models/person.dart';
+import 'package:tether/services/relationship_health_service.dart';
+
+void main() {
+  const service = RelationshipHealthService();
+
+  test('classifies a thriving relationship', () {
+    const person = Person(id: '1', name: 'A', initials: 'A', bondXp: 900, recentInteractions: 6, cadence: Cadence.daily, state: BondState.thriving);
+    expect(service.classify(person), BondState.thriving);
+  });
+
+  test('classifies a relationship needing attention', () {
+    const person = Person(id: '2', name: 'B', initials: 'B', bondXp: 0, recentInteractions: 0, cadence: Cadence.weekly, state: BondState.needsAttention);
+    expect(service.classify(person), BondState.needsAttention);
+  });
+}
